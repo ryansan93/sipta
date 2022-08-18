9a61962a591638718d5264b0b29a96a9d06ff914
ryansan93 <ryansantoso93@gmail.com>
2022-08-18 12:43:46 +0700

    update 20220818

--- application/modules/laporan/views/pengajuan/list_report_by_prodi.php
+++ application/modules/laporan/views/pengajuan/list_report_by_prodi.php
@@ -10,7 +10,7 @@
 					<div class="col-xs-10 no-padding"><span>: <?php echo $v_det['jumlah']; ?></span></div>
 				</div>
 			<?php endforeach ?>
-			<div class="col-xs-3 no-padding"><hr style="margin-top: 10px; margin-bottom: 10px"></div>
+			<div class="col-xs-3 no-padding" style="margin-top: 10px; margin-bottom: 10px;"></div>
 			<div class="col-xs-12 no-padding">
 				<div class="col-xs-2 no-padding">
 					<span>PENGAJUAN DI TERIMA</span>
@@ -24,7 +24,7 @@
 				<div class="col-xs-10 no-padding"><span>: <?php echo isset($value['pengajuan_selesai']) ? angkaRibuan($value['pengajuan_selesai']) : 0; ?></span></div>
 			</div>
 		</div>
-		<div class="col-xs-12 no-padding"><hr style="margin-top: 10px; margin-bottom: 10px"></div>
+		<!-- <div class="col-xs-12 no-padding"><hr style="margin-top: 10px; margin-bottom: 10px;"></div> -->
 	<?php endforeach ?>
 <?php else: ?>
 	<h3>Data tidak ditemukan.</h3>
