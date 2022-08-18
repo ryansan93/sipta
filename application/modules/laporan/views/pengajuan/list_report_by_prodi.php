c9bf6be4bfff2654e4f09702d20726294a895c53
ryansan93 <ryansantoso93@gmail.com>
2022-08-18 12:42:26 +0700

    update 20220818

--- application/modules/laporan/views/pengajuan/list_report_by_prodi.php
+++ application/modules/laporan/views/pengajuan/list_report_by_prodi.php
@@ -5,23 +5,23 @@
 			<?php foreach ($value['detail'] as $k_det => $v_det): ?>
 				<div class="col-xs-12 no-padding">
 					<div class="col-xs-2 no-padding">
-						<span><b><?php echo $v_det['nama']; ?></b></span>
+						<span><?php echo $v_det['nama']; ?></span>
 					</div>
-					<div class="col-xs-10 no-padding"><span><b>: <?php echo $v_det['jumlah']; ?></b></span></div>
+					<div class="col-xs-10 no-padding"><span>: <?php echo $v_det['jumlah']; ?></span></div>
 				</div>
 			<?php endforeach ?>
 			<div class="col-xs-3 no-padding"><hr style="margin-top: 10px; margin-bottom: 10px"></div>
 			<div class="col-xs-12 no-padding">
 				<div class="col-xs-2 no-padding">
-					<span><b>PENGAJUAN DI TERIMA</b></span>
+					<span>PENGAJUAN DI TERIMA</span>
 				</div>
-				<div class="col-xs-10 no-padding"><span><b>: <?php echo isset($value['pengajuan_masuk']) ? angkaRibuan($value['pengajuan_masuk']) : 0; ?></b></span></div>
+				<div class="col-xs-10 no-padding"><span>: <?php echo isset($value['pengajuan_masuk']) ? angkaRibuan($value['pengajuan_masuk']) : 0; ?></span></div>
 			</div>
 			<div class="col-xs-12 no-padding">
 				<div class="col-xs-2 no-padding">
-					<span><b>PENGAJUAN SELESAI</b></span>
+					<span>PENGAJUAN SELESAI</span>
 				</div>
-				<div class="col-xs-10 no-padding"><span><b>: <?php echo isset($value['pengajuan_selesai']) ? angkaRibuan($value['pengajuan_selesai']) : 0; ?></b></span></div>
+				<div class="col-xs-10 no-padding"><span>: <?php echo isset($value['pengajuan_selesai']) ? angkaRibuan($value['pengajuan_selesai']) : 0; ?></span></div>
 			</div>
 		</div>
 		<div class="col-xs-12 no-padding"><hr style="margin-top: 10px; margin-bottom: 10px"></div>
